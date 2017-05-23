deae6fa63ef3b44dd403764335a0138a820864f7
Ash Wilson <smashwilson@gmail.com>
2017-05-22 22:08:49 -0400

    Unnecessary if clause

--- provision/cleanup.py
+++ provision/cleanup.py
@@ -19,7 +19,7 @@ def rollback(context):
             had_failure = True
             error('unable to delete key pair\n{}'.format(traceback.format_exc()))
 
-    if context.instance and context.security_group:
+    if context.instance:
         try:
             info('revoking RDS security group ingress rule')
             rds_security_group = ec2.SecurityGroup(context.config.rds_security_group_id)
@@ -34,7 +34,6 @@ def rollback(context):
             had_failure = True
             error('unable to remove RDS security group rule\n{}'.format(traceback.format_exc()))
 
-    if context.instance:
         try:
             info('terminating instance')
             context.instance.terminate()
@@ -57,4 +56,56 @@ def rollback(context):
         error('one or more resources are still live on AWS')
     else:
         success('all resources deleted from AWS')
+    return had_failure
+
+def retire(context):
+    """
+    Delete any resources on the AWS account created by previous runs.
+    """
+
+    had_failure = False
+
+    tag_filter = [{'Name': 'tag:purpose', 'Values': ['pushbot']}]
+    pushbot_instances = ec2.instances.filter(Filters=tag_filter)
+    pushbot_security_groups = ec2.security_groups.filter(Filters=tag_filter)
+    pushbot_key_pairs = ec2.key_pairs.filter(Filters=[{'Name': 'key-name', 'Values': ['pushbot*']}])
+
+    prior_instances = [i for i in pushbot_instances if i.id != context.instance.id]
+    prior_security_groups = [g for g in pushbot_security_groups if g.id != context.security_group.id]
+    prior_key_pairs = [k for k in pushbot_key_pairs if k.name != context.key_pair.name]
+
+    info('deleting {} prior key pairs'.format(len(prior_key_pairs)))
+    for k in prior_key_pairs:
+        try:
+            k.delete()
+        except:
+            had_failure = True
+            error('unable to delete key pair\n{}'.format(traceback.format_exc()))
+
+    info('terminating {} prior instances'.format(len(prior_instances)))
+    for i in prior_instances:
+        try:
+            i.terminate()
+        except:
+            had_failure = True
+            error('unable to terminate instance\n{}'.format(traceback.format_exc()))
+    for i in prior_instances:
+        try:
+            i.wait_until_terminated()
+        except:
+            had_failure = True
+            error('unable to wait for instance to terminate\n{}'.format(traceback.format_exc()))
+
+    info('deleting {} prior security groups'.format(len(prior_security_groups)))
+    for g in prior_security_groups:
+        try:
+            g.delete()
+        except:
+            had_failure = True
+            error('unable to delete security group\n{}'.format(traceback.format_exc()))
+
+    if had_failure:
+        error('one or more prior resources are still live on AWS')
+    else:
+        success('all prior resources deleted')
     return had_failure
