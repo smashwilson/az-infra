5cd18ce1ad22000764c9a7bfc09ff6d073652747
Ash Wilson <smashwilson@gmail.com>
2017-05-28 18:24:54 -0400

    Exit with 1 if the deployment failed

--- provision/__main__.py
+++ provision/__main__.py
@@ -21,8 +21,10 @@ if context.config.action == Action.PROVISION:
         error('provisioning error\n{}'.format(formatted_tb))
         notify.failure(context, formatted_tb)
         cleanup.rollback(context)
+        sys.exit(1)
 elif context.config.action == Action.DELETE:
     try:
         cleanup.retire(context)
     except:
         error('unable to delete resources\n{}'.format(traceback.format_exc()))
+        sys.exit(1)
