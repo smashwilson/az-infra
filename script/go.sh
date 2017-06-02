3619036707e9616c9eaa6afcf420729203a41dee
Ash Wilson <smashwilson@gmail.com>
2017-06-02 09:38:19 -0400

    Port scripts to bash

---
+++ script/go.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Provision a new deployment using the credentials in credentials.ps1.
+
+source ./credentials.sh
+
+docker build -t azurefire-infra:local .
+clear
+docker run --rm \
+  -e AWS_ACCESS_KEY_ID \
+  -e AWS_SECRET_ACCESS_KEY \
+  -e AWS_DEFAULT_REGION \
+  -e POSTGRES_URL \
+  -e SLACK_TOKEN \
+  -e DARKSKY_APIKEY \
+  -e GOOGLE_CSE_ID \
+  -e GOOGLE_CSE_KEY \
+  -e LE_EMAIL \
+  -e SLACK_WEBHOOK_URL \
+  -e MAGICAL_WEAK_SPOT_TOKEN \
+  -v C:/Users/smash/src/azurefire-infra/out:/usr/src/app/out:rw \
+  azurefire-infra:local "$@"
