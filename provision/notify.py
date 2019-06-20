7e5b38c5feaf299d8193b62da95535121372a1f2
Ash Wilson <smashwilson@github.com>
2019-06-20 10:40:32 -0400

    Remove requests dependency

--- provision/notify.py
+++ provision/notify.py
@@ -1,40 +1,54 @@
-import requests
+import urllib.request
+import json
 import os
 
 def begin(context):
-    webhook_url = context.config.slack_webhook_url
-    requests.post(webhook_url, json={
-        'text': 'Infrastructure deployment is <now in progress|https://github.com/smashwilson/az-infra/actions>.'
-    })
+    req = urllib.request.Request(
+        method='POST',
+        url=context.config.slack_webhook_url,
+        headers={'Content-Type': 'application/json'},
+        data=json.dumps({
+            'text': 'Infrastructure deployment is <now in progress|https://github.com/smashwilson/az-infra/actions>.'
+        })
+    )
+    urllib.request.urlopen(req)
 
 def success(context):
-    webhook_url = context.config.slack_webhook_url
-
-    requests.post(webhook_url, json={
-        'attachments': [
-            {
-                'fallback': 'azurefire infrastructure deployed successfully.',
-                'color': 'good',
-                'title': 'Azurefire infrastructure deployed successfully.',
-                'title_link': 'https://github.com/smashwilson/az-infra/actions',
-                'text': 'Server `{}` launched and bootstrapped in _{}_.'.format(context.instance.id, context.elapsed_time()),
-                'mrkdwn_in': ['text']
-            }
-        ]
-    })
+    req = urllib.request.Request(
+        method='POST',
+        url=context.config.slack_webhook_url,
+        headers={'Content-Type': 'application/json'},
+        data=json.dumps({
+            'attachments': [
+                {
+                    'fallback': 'azurefire infrastructure deployed successfully.',
+                    'color': 'good',
+                    'title': 'Azurefire infrastructure deployed successfully.',
+                    'title_link': 'https://github.com/smashwilson/az-infra/actions',
+                    'text': 'Server `{}` launched and bootstrapped in _{}_.'.format(context.instance.id, context.elapsed_time()),
+                    'mrkdwn_in': ['text']
+                }
+            ]
+        })
+    )
+    urllib.request.urlopen(req)
 
 def failure(context, formatted_tb):
-    webhook_url = context.config.slack_webhook_url
-
-    requests.post(webhook_url, json={
-        'attachments': [
-            {
-                'fallback': 'azurefire infrastructure deployment failed.',
-                'color': 'danger',
-                'title': 'Azurefire infrastructure deployment failed.',
-                'title_link': context.build_href(),
-                'text': "Here's the stack:\n```\n{}\n```\n".format(formatted_tb),
-                'mrkdwn_in': ['text']
-            }
-        ]
-    })
+    req = urllib.request.Request(
+        method='POST',
+        url=context.config.slack_webhook_url,
+        headers={'Content-Type': 'application/json'},
+        data=json.dumps({
+            'attachments': [
+                {
+                    'fallback': 'azurefire infrastructure deployment failed.',
+                    'color': 'danger',
+                    'title': 'Azurefire infrastructure deployment failed.',
+                    'title_link': context.build_href(),
+                    'text': "Here's the stack:\n```\n{}\n```\n".format(formatted_tb),
+                    'mrkdwn_in': ['text']
+                }
+            ]
+        })
+    )
+    urllib.request.urlopen(req)
