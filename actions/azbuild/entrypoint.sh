a82ff9a0b92b47e19a55d232e9e8d4b2593fcf34
Ash Wilson <smashwilson@github.com>
2019-07-03 10:50:42 -0400

    Apply labels to built container images

--- actions/azbuild/entrypoint.sh
+++ actions/azbuild/entrypoint.sh
@@ -18,7 +18,12 @@ printf "Authenticating to Docker registry.\n"
 docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD} ${DOCKER_REGISTRY_URL}
 
 printf "Bulding Docker container (%s:%s).\n" "${IMAGE_NAME}" "${IMAGE_TAG}"
-docker build -t "${IMAGE_NAME}:${IMAGE_TAG}" .
+docker build \
+  --label net.azurefire.repository=${GITHUB_REPOSITORY} \
+  --label net.azurefire.commit=${GITHUB_SHA} \
+  --label net.azurefire.ref=${GITHUB_REF##refs/heads/} \
+  -t "${IMAGE_NAME}:${IMAGE_TAG}" \
+  .
 
 printf "Pushing Docker container.\n"
 docker push "${IMAGE_NAME}:${IMAGE_TAG}"
